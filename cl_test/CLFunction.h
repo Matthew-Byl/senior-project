436903bf9e72d725fc63356132f0a429e51207d2
John Kloosterman <jsk9@students.calvin.edu>
2012-12-13 13:07:50 -0500

    Allow running bare kernels.

--- cl_test/CLFunction.h
+++ cl_test/CLFunction.h
@@ -13,18 +13,14 @@ class CLFunction
 public:
 	CLFunction( std::string function, 
 				std::string kernel, 
-				const CLContext context = CLContext(0, 0 ) )
-		: myContext( context ), myFunction( function), myKernel( kernel ) 
+				bool isKernel = false,
+				const CLContext context = CLContext( 0, 0 ) )
+		: myContext( context ), 
+		  myFunction( function), 
+		  myKernel( kernel ), 
+		  myIsKernel( isKernel)
 		{
 		}
-/*
-	CLFunction( std::string function, std::string kernel )
-		: myFunction( function), myKernel( kernel ) 
-		{
-			myContext = CLContext( 0, 0 );
-		}
-*/
-//	void addArgument( CLUnitArgument argument );
 
 	template<class ...Arguments>
 	void setArguments( Arguments... params )
@@ -47,25 +43,47 @@ public:
 			setArguments( params... );
 			return run();
 		}
+	
 private:
 	const CLContext myContext;
 	std::vector<CLUnitArgument> myArguments;
 	std::string myFunction;
 	std::string myKernel;
+	bool myIsKernel;
 };
 
 
-template<>
-const int CLFunction<int>::run()
-{
-	return run( "int" );
-}
+/*
+ * Generate run() methods for every standard type.
+ */
+#define _GEN_CL_FUNCTION_RUN( host, kernel ) \
+	template<>								 \
+	const host CLFunction<host>::run() \
+	{									   \
+		return run( #kernel );			   \
+	}
 
+_GEN_CL_FUNCTION_RUN( cl_int, int )
+_GEN_CL_FUNCTION_RUN( cl_float, float )
+
+
+/// @todo: specialize for void, when we return nothing; i.e. run a kernel.
 template<class T>
 const T CLFunction<T>::run( std::string type )
 {
-    KernelGenerator generator( myFunction, myArguments, type );
-	std::string src( myKernel + "\n\n" + generator.generate() );
+	std::string src;
+	std::string kernelFunction;
+	if ( myIsKernel )
+	{
+		src = myKernel;
+		kernelFunction = myFunction;
+	}
+	else
+	{	
+		KernelGenerator generator( myFunction, myArguments, type );
+		src = myKernel + "\n\n" + generator.generate();
+		kernelFunction = generator.getKernelFunction();
+	}
 
 	std::cout << "FULL SOURCE" << std::endl;
 	std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
@@ -75,7 +93,7 @@ const T CLFunction<T>::run( std::string type )
 	cl::Program program = myContext.buildProgram( src );
 	cl::Kernel kernel(
         program,
-        generator.getKernelFunction().c_str()
+        kernelFunction.c_str()
 	);
 
 	// Assemble a vector of buffers.
@@ -100,16 +118,20 @@ const T CLFunction<T>::run( std::string type )
 		it.enqueue( queue );
 	}
 
-	// Create the buffer for the result.
 	T result;
-	cl::Buffer resultBuffer(
-		myContext.getContext(),
-		CL_MEM_WRITE_ONLY,
-		sizeof( T ),
-		NULL
-		);
-	// Make it the next kernel argument.
-	kernel.setArg( myArguments.size(), resultBuffer );
+	cl::Buffer resultBuffer;
+	if ( !myIsKernel )
+	{
+		// Create the buffer for the result.
+		resultBuffer = cl::Buffer(
+			myContext.getContext(),
+			CL_MEM_WRITE_ONLY,
+			sizeof( T ),
+			NULL
+			);
+		// Make it the next kernel argument.
+		kernel.setArg( myArguments.size(), resultBuffer );
+	}
 
 	// Queue up the kernel.
 	cl::NDRange globalWorkSize( 1 );
@@ -122,15 +144,20 @@ const T CLFunction<T>::run( std::string type )
 		NULL
 	);
 
-	// Enqueue reading the result.
-	queue.enqueueReadBuffer(
-		resultBuffer,
-		CL_TRUE,
-		0,
-		sizeof( T ),
-		&result,
-		NULL,
-		NULL );
+	if ( !myIsKernel )
+	{
+		// Enqueue reading the result.
+		queue.enqueueReadBuffer(
+			resultBuffer,
+			CL_TRUE,
+			0,
+			sizeof( T ),
+			&result,
+			NULL,
+			NULL );
+	}
+
+	// Enqueue reading all the results back.
 
 	return result;
 }
--- cl_test/main.cpp
+++ cl_test/main.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 
 const char *src = 
+	" __kernel void some_kernel( __global int *i, __global int3 *j ) { printf( \"From kernel: %d\", *i + (*j).x ); } \n" \
 	"int add_one( int i, int3 j ) " \
 	"{" \
 	"   printf(\"From OpenCL: %d %d\\n\", i, j.x ); " \
@@ -14,16 +15,15 @@ const char *src =
 
 int main( void )
 {
-//	CLContext context( 0, 0 );
 	CLFunction<int> add_one( "add_one", src );
+	CLFunction<int> some_kernel( "some_kernel", src, true );
+
 	cl_int3 intr;
 	intr.s[0] = 2;
 	cl_int result;
 
-//	add_one.setArguments( 1, intr );
-//	result = add_one.run();
-
 	result = add_one( 1, intr );
-
 	printf( "Result: %d\n", result );
+
+	some_kernel( 1, intr );
 }
