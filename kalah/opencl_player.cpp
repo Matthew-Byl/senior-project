5fe803d0da2d37ef6912779b7a5bac60f92e5622
John Kloosterman <jsk9@students.calvin.edu>
2012-12-28 18:48:30 -0500

    Object-orientize a bit.

--- kalah/opencl_player.cpp
+++ kalah/opencl_player.cpp
@@ -5,19 +5,100 @@ extern "C" {
 
 #include <CLKernel.h>
 
+#include <cmath>
 #include <fstream>
 #include <string>
 using namespace std;
 
-/*
 extern "C" const char *opencl_player_name( void )
 {
 	return "OpenCL Minimax";
 }
 
+int ipow( int n, int k )
+{
+	return (int) pow( n, k );
+}
+
+int get_leaf_nodes( int sequentialDepth )
+{
+	// The number of leaf nodes of the given sequential depth.
+	return ipow( 6, sequentialDepth );
+}
+
+int get_board_array_size( int sequentialDepth )
+{
+	return get_leaf_nodes( sequentialDepth ) * 6 * 6 *6;
+}
+
+class OpenCLPlayer
+{
+public:
+	OpenCLPlayer( Board start, int sequentialDepth, string src )
+		: mySequentialDepth( sequentialDepth ),
+		  myBoardsSize( get_board_array_size( sequentialDepth ) ),
+		  myBoards( new Board[myBoardsSize] ),
+		  startBoard( start ),
+		  generate_boards( "generate_boards", src ),
+		  evaluate_board( "evaluate_board", src ),
+		  minimax( "minimax", src ),
+		  host_boards( "Board", myBoards, myBoardsSize ),
+		  start_boards( "Board", start )
+		{
+			cout << "Board size: " << get_board_array_size( sequentialDepth ) << endl;
+		};
+
+	~OpenCLPlayer()
+		{
+			delete[] myBoards;
+		};
+
+	int makeMove()
+		{
+			generate_boards.setGlobalDimensions( 1, 216 );
+			generate_boards.setLocalDimensions( 1, 216 ); // this needs to stay with x-dimension 1
+			generate_boards( start_boards, host_boards );
+			
+			evaluate_board.setGlobalDimensions( 1, 216, 14 );
+			evaluate_board( host_boards );
+			
+			minimax.setGlobalDimensions( 1, 42 );
+			minimax.setLocalDimensions( 1, 42 ); // this needs to stay with x-dimension 1.
+			minimax( host_boards );
+			/*	
+			for ( int i = 0; i < 258; i++ )
+			{
+				printf( "*** %d ***\n" , i );
+				board_print( &myBoards[i] );
+				printf( "\n" );
+				}*/
+
+			return 0;
+		}
+
+private:
+	int mySequentialDepth;
+	int myBoardsSize;
+	Board *myBoards;
+	Board startBoard;
+
+	CLContext myContext;
+	CLKernel generate_boards;
+	CLKernel evaluate_board;
+	CLKernel minimax;
+
+	CLUnitArgument host_boards;
+	CLUnitArgument start_boards;
+};
+
 extern "C" int opencl_player_move( Board *b )
 {
-	return 0;
+	ifstream t("opencl_player.cl");
+    string src((std::istreambuf_iterator<char>(t)),
+               std::istreambuf_iterator<char>());
+	
+	OpenCLPlayer player( *b, 1, src );
+	return player.makeMove();
 }
 
 KalahPlayer opencl_player( void )
@@ -29,9 +110,9 @@ KalahPlayer opencl_player( void )
 
 	return k;
 }
-*/
 
 #ifdef MAKE_MAIN
+
 int main ( void )
 {
 	Board b;
@@ -40,33 +121,9 @@ int main ( void )
 	ifstream t("opencl_player.cl");
     string src((std::istreambuf_iterator<char>(t)),
                std::istreambuf_iterator<char>());
-
-	Board boards[42];
-	CLUnitArgument board_array( "Board", boards, 42 );
-	CLUnitArgument board( "Board", b );
-
-	CLContext ctx( 1, 0 );
-	CLKernel generate_boards( "generate_boards", src );
-	generate_boards.setGlobalDimensions( 1, 42 );
-	generate_boards.setLocalDimensions( 1, 42 );
-	generate_boards( board, board_array );
-
-	CLKernel evaluate_board( "evaluate_board", src );
-	evaluate_board.setGlobalDimensions( 1, 36, 14 );
-	evaluate_board( board_array );
-
-	CLKernel minimax( "minimax", src );
-	minimax.setGlobalDimensions( 1, 6 );
-	minimax.setLocalDimensions( 1, 6 );
-	minimax( board_array );
-
-
-	for ( int i = 0; i < 42; i++ )
-	{
-		printf( "*** %d ***\n" , i );
-		board_print( &boards[i] );
-		printf( "\n" );
-	}
+	
+	OpenCLPlayer player( b, 1, src );
+	player.makeMove();
 
 }
 #endif
