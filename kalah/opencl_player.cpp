104c2652bad1b413a6d9f942c658fe21389918b7
John Kloosterman <jsk9@students.calvin.edu>
2012-12-28 19:21:17 -0500

    Generate start boards.

--- kalah/opencl_player.cpp
+++ kalah/opencl_player.cpp
@@ -28,7 +28,9 @@ int get_leaf_nodes( int sequentialDepth )
 
 int get_board_array_size( int sequentialDepth )
 {
-	return get_leaf_nodes( sequentialDepth ) * 6 * 6 *6;
+	int leaf = get_leaf_nodes( sequentialDepth );
+
+	return ( leaf * 6 ) + ( leaf * 6 * 6 ) + ( leaf * 6 * 6 * 6 );
 }
 
 class OpenCLPlayer
@@ -37,15 +39,16 @@ public:
 	OpenCLPlayer( Board start, int sequentialDepth, string src )
 		: mySequentialDepth( sequentialDepth ),
 		  myBoardsSize( get_board_array_size( sequentialDepth ) ),
+		  myStartBoard( start ),
 		  myBoards( new Board[myBoardsSize] ),
-		  startBoard( start ),
+		  myStartBoards( new Board[ get_leaf_nodes( mySequentialDepth ) ] ),
 		  generate_boards( "generate_boards", src ),
 		  evaluate_board( "evaluate_board", src ),
 		  minimax( "minimax", src ),
 		  host_boards( "Board", myBoards, myBoardsSize ),
-		  start_boards( "Board", start )
+		  start_boards( "Board", myStartBoards, get_leaf_nodes( mySequentialDepth ) )
 		{
-			cout << "Board size: " << get_board_array_size( sequentialDepth ) << endl;
+
 		};
 
 	~OpenCLPlayer()
@@ -53,8 +56,52 @@ public:
 			delete[] myBoards;
 		};
 
+	int leaf_start ( void )
+		{
+			int half_6ary_tree_size = ( ( (int) pow( 6.0f, mySequentialDepth + 1 ) - 1 ) / ( 6 - 1 ) );
+			return half_6ary_tree_size - 1;
+		}
+
+	void generate_board( Board parent, int idx, int depth )
+		{
+			if ( depth == 0 )
+			{
+				printf( "Leaf start: %d\n", leaf_start() );
+
+				myStartBoards[idx - leaf_start()] = parent;
+				return;
+			}
+
+			int child_idx = 6 * ( idx + 1 );
+			for ( int i = 0; i < 6; i++ )
+			{
+				int move_offset = 0;
+				Board m_board = parent;
+
+				if ( m_board.player_to_move == TOP )
+					move_offset = 7;
+
+				if ( board_legal_move( &m_board, i + move_offset ) )
+				{
+					board_make_move( &m_board, i + move_offset );
+				}
+
+				generate_board( m_board, child_idx + i, depth - 1 );
+			}
+		}
+
+	void generate_start_boards()
+		{
+			generate_board( myStartBoard, 0, mySequentialDepth );
+		};
+
 	int makeMove()
 		{
+			int num_leaf_nodes = get_leaf_nodes( mySequentialDepth );
+
+			// Create start boards
+			generate_start_boards();
+
 			generate_boards.setGlobalDimensions( 1, 216 );
 			generate_boards.setLocalDimensions( 1, 216 ); // this needs to stay with x-dimension 1
 			generate_boards( start_boards, host_boards );
@@ -79,8 +126,9 @@ public:
 private:
 	int mySequentialDepth;
 	int myBoardsSize;
+	Board myStartBoard;
 	Board *myBoards;
-	Board startBoard;
+	Board *myStartBoards;
 
 	CLContext myContext;
 	CLKernel generate_boards;
@@ -122,7 +170,7 @@ int main ( void )
     string src((std::istreambuf_iterator<char>(t)),
                std::istreambuf_iterator<char>());
 	
-	OpenCLPlayer player( b, 1, src );
+	OpenCLPlayer player( b, 2, src );
 	player.makeMove();
 
 }
