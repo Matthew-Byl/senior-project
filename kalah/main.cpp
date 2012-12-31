55c2eb2cef68c756c309637d8685474d53aa7a18
John Kloosterman <jsk9@students.calvin.edu>
2012-12-30 23:55:23 -0500

    Handle case where game ends in parallel part.

--- kalah/main.cpp
+++ kalah/main.cpp
@@ -96,7 +96,7 @@ void match ( KalahPlayer top, KalahPlayer bottom )
 int main ( void )
 {
 	KalahPlayer bottom = bonzo_player();
-	KalahPlayer top = minimax_player();
+	KalahPlayer top = opencl_minimax_player();
 	match( top, bottom );
 
 	return 0;
