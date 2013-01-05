01aa4dc4c1bf30b804f26a28eec5b11e8d6bd903
John Kloosterman <jsk9@students.calvin.edu>
2013-01-05 14:06:19 -0500

    Minimax tests.

--- kalah/opencl_player_tests.cpp
+++ kalah/opencl_player_tests.cpp
@@ -9,6 +9,7 @@ extern "C" {
 #include <iostream>
 #include <cassert>
 #include <cstdlib>
+#include <climits>
 using namespace std;
 
 #define NUM_ITERATIONS 512
@@ -89,7 +90,6 @@ void test_generate_boards( string src )
 	generate_boards_test.setGlobalDimensions( 1, 216 );
 	generate_boards_test.setLocalDimensions( 1, 216 );
 
-
 	for ( int i = 0; i < NUM_ITERATIONS; i++ )
 	{
 		start_board = generate_valid_board();
@@ -139,6 +139,64 @@ void test_evaluate_boards( string src )
 	cout << endl << "All tests passed." << endl;
 }
 
+int check_minimax( Board *opencl_boards, int idx )
+{
+	if ( idx > 42 )
+		return opencl_boards[idx].score;
+
+	int best_score;
+	if ( opencl_boards[idx].player_to_move == TOP )
+		best_score = INT_MIN;
+	else
+		best_score = INT_MAX;
+
+	int child_offset = tree_array_first_child( 6, idx );
+	for ( int i = 0; i < 7; i++ )
+	{
+		int score = check_minimax( opencl_boards, child_offset + i );
+
+		if ( ( opencl_boards[idx].player_to_move == TOP && score > best_score )
+			 || ( opencl_boards[idx].player_to_move == BOTTOM && score < best_score ) )
+		{
+			best_score = score;
+		}
+	}
+
+	assert( opencl_boards[idx].score == best_score );
+	return best_score;
+}
+
+void test_minimax( string src )
+{
+	cout << "Testing minimax..." << endl;
+
+	Board boards[259];
+
+	CLKernel minimax_test( "minimax_test", src );
+	CLUnitArgument host_boards( "Board", boards, 259 );
+	vector<CLUnitArgument> args;
+	args.push_back( host_boards );
+	minimax_test.setGlobalDimensions( 1, 216 );
+	minimax_test.setLocalDimensions( 1, 216 );
+
+	// Generate a random array, put scores in the bottom,
+	//  randomly mark some nodes as illegal moves, and go.
+	for ( int i = 0; i < 259; i++ )
+	{
+		boards[i] = generate_valid_board();
+		if ( ( rand() % 5 ) == 0 )
+			boards[i].legal_move = FALSE;
+	}
+
+	for ( int i = 43; i < 259; i++ )
+		boards[i].score = minimax_eval( &boards[i] );
+
+	minimax_test( args );
+	check_minimax( boards, 0 );
+
+	cout << endl << "All tests passed." << endl;
+}
+
 int main ( void )
 {
 	srand( time( NULL ) );
@@ -157,6 +215,7 @@ int main ( void )
 */
 	test_generate_boards( src );
 	test_evaluate_boards( src );
+	test_minimax( src );
 
 	return 0;
 }
