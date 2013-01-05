e52949cb9db4e18206dcd71c87bec71f4e6aaf5d
John Kloosterman <jsk9@students.calvin.edu>
2013-01-05 18:49:14 -0500

    Undo bad commit.

--- kalah/depths.h
+++ kalah/depths.h
@@ -3,7 +3,7 @@
 #define WORKGROUP_SIZE 1000000
 
 #define PRE_DEPTH 1
-#define SEQUENTIAL_DEPTH 9
+#define SEQUENTIAL_DEPTH 3
 #define PARALLEL_DEPTH 4
 // Actual tree depth:
 //  MINIMAX_DEPTH = SEQUENTIAL_DEPTH + PARALLEL_DEPTH - 1 (one of the parallel levels overlaps with the sequential levels)
