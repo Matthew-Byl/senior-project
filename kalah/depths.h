018c455ec619aaf3d3da0e1aee74884920c736f8
John Kloosterman <jsk9@students.calvin.edu>
2013-01-05 19:17:39 -0500

    Smoke Bonzo.

--- kalah/depths.h
+++ kalah/depths.h
@@ -1,9 +1,9 @@
 #ifndef _DEPTHS_H
 
-#define WORKGROUP_SIZE 1000000
+#define WORKGROUP_SIZE 500000
 
-#define PRE_DEPTH 1
-#define SEQUENTIAL_DEPTH 3
+#define PRE_DEPTH 2
+#define SEQUENTIAL_DEPTH 8
 #define PARALLEL_DEPTH 4
 // Actual tree depth:
 //  MINIMAX_DEPTH = SEQUENTIAL_DEPTH + PARALLEL_DEPTH - 1 (one of the parallel levels overlaps with the sequential levels)
--- kalah/evaluate.c
+++ kalah/evaluate.c
@@ -3,7 +3,6 @@
 int minimax_eval( Board *b )
 {
 	// Kalah counts double, but stones count too.
-/*
 	int score = 5 * ( b->board[13] - b->board[6] );
 	
 	for ( int i = 0; i <= 5; i++ )
@@ -12,9 +11,5 @@ int minimax_eval( Board *b )
 	for ( int i = 7; i <= 12; i++ )
 		score += b->board[i];
 
-	num_evaled++;
 	return score;
-*/
-
-	return b->board[13] - b->board[6];
 }
