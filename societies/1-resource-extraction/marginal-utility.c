de3d624c1977f728a9f59a4c91b6ff322dd1a26c
John Kloosterman <jsk9@students.calvin.edu>
2013-01-18 14:56:53 -0500

    Add report TeX file.

--- societies/1-resource-extraction/marginal-utility.c
+++ societies/1-resource-extraction/marginal-utility.c
@@ -2,3 +2,35 @@
  * Functions to compute economic utility.
  */
 
+/**
+ * Computes MU(x) = (D/n) * x^((1/n) - 1 ).
+ *  See page 5 of the paper.
+ *
+ * @param x
+ *  The number of units of the resource already owned.
+ * @param D
+ *  The level of utility for the resource.
+ * @param n
+ *  The diminishing marginal utility for the resource.
+ */
+float mu( int x, int D, int n )
+{
+	float C = (float) D / (float) n;
+
+	float exponent = ( 1.0f / (float) x ) - 1.0f;
+	return C * pow( (float) x, exponent );
+}
+
+/**
+ * Compute the gain per minute for a given resource.
+ *   See page 7 of the paper.
+ *
+ * @param x
+ *  The number of units of the resource already owned.
+ * @param e
+ *  The agent's current effort to extract the resource.
+ */
+float gpm( int x, float e )
+{
+	
+}
---
+++ societies/1-resource-extraction/resource-extraction.c
@@ -0,0 +1,7 @@
+/**
+ * Simulate one day's worth of collecting resources.
+ *
+ * Approaches:
+ *  -Loop for each minute
+ *  -Loop that stores time left and what we got. (fewer iterations)
+ */
