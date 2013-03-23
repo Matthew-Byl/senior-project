c26173d750a8eff97e3ea6442ee7edd548b698d4
John Kloosterman <john.kloosterman@gmail.com>
2013-03-23 19:02:42 -0400

    Move all Clang code into a separate class.

---
+++ local_malloc/ClangInterface.cpp
@@ -0,0 +1,123 @@
+#include "ClangInterface.h"
+
+//-------------------------------------------------------------------------
+//
+// rewritersample.cpp: Source-to-source transformation sample with Clang,
+// using Rewriter - the code rewriting interface.
+//
+// Eli Bendersky (eliben@gmail.com)
+// This code is in the public domain
+//
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <cassert>
+
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Basic/Diagnostic.h"
+#include "clang/Basic/FileManager.h"
+#include "clang/Basic/SourceManager.h"
+#include "clang/Basic/TargetOptions.h"
+#include "clang/Basic/TargetInfo.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Lex/Preprocessor.h"
+#include "clang/Parse/ParseAST.h"
+#include "clang/Rewrite/Core/Rewriter.h"
+#include "clang/Rewrite/Frontend/Rewriters.h"
+#include "llvm/Support/Host.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/ADT/APSInt.h"
+
+#include "CallGraph.h"
+#include "AllocationAST.h"
+
+using namespace clang;
+using namespace std;
+
+ClangInterface::ClangInterface( string fileName )
+{
+	cout << "File: " << fileName << endl;
+
+    myCompilerInstance.createDiagnostics(0, 0);
+
+	// Target info
+    TO.Triple = llvm::sys::getDefaultTargetTriple();
+    TargetInfo *TI = TargetInfo::CreateTargetInfo(
+        myCompilerInstance.getDiagnostics(), TO);
+    myCompilerInstance.setTarget(TI);
+
+	// Invocation, including all the headers necessary for
+	//  OpenCL support.
+	const char * const options[] = {
+		"-x", "cl",
+		"-I", "libclc/generic/include",
+		"-include", "clc/clc.h",
+		"-I", "/usr/include/clang/3.0/include"
+	};
+	CompilerInvocation::CreateFromArgs(
+		invocation,
+		options,
+		options + 8,
+		myCompilerInstance.getDiagnostics()
+		);	 
+	myCompilerInstance.setInvocation( &invocation );
+
+	// Files
+    myCompilerInstance.createFileManager();
+    FileManager &FileMgr = myCompilerInstance.getFileManager();
+    myCompilerInstance.createSourceManager(FileMgr);
+    SourceManager &SourceMgr = myCompilerInstance.getSourceManager();
+    myCompilerInstance.createPreprocessor();
+    myCompilerInstance.createASTContext();
+
+    // A Rewriter helps us manage the code rewriting task.
+    myRewriter.setSourceMgr(SourceMgr, myCompilerInstance.getLangOpts());
+
+    // Set the main file handled by the source manager to the input file.
+    const FileEntry *FileIn = FileMgr.getFile( fileName );
+    SourceMgr.createMainFileID(FileIn);
+    myCompilerInstance.getDiagnosticClient().BeginSourceFile(
+        myCompilerInstance.getLangOpts(),
+        &myCompilerInstance.getPreprocessor());
+}
+
+void ClangInterface::processAST( ASTConsumer *astConsumer )
+{
+   ParseAST(
+	   myCompilerInstance.getPreprocessor(), 
+	   astConsumer,
+	   myCompilerInstance.getASTContext()
+	   );
+}
+
+int main ( int argc, char *argv[] )
+{
+	assert( argc == 2 );
+
+	ClangInterface clangInterface( argv[1] );
+
+	CallGraph callGraph;
+	AllocationASTConsumer allocationConsumer( 
+		clangInterface.getRewriter(),
+		callGraph,
+		clangInterface.getASTContext()
+		);
+
+	clangInterface.processAST( &allocationConsumer );
+
+	int max_alloc = callGraph.maximum_alloc( "entry" );
+	cout << "Maximum allocation: " << max_alloc << endl;
+ 
+    // At this point the rewriter's buffer should be full with the rewritten
+    // file contents.
+//    const RewriteBuffer *RewriteBuf =
+	//      TheRewriter.getRewriteBufferFor(SourceMgr.getMainFileID());
+//	llvm::outs() << string(RewriteBuf->begin(), RewriteBuf->end());
+
+
+	exit( 0 );
+	// There is a bug with the destructor. Solve it later.
+    return 0;
+}
---
+++ local_malloc/ClangInterface.h
@@ -0,0 +1,34 @@
+#ifndef _CLANG_INTERFACE_H
+#define _CLANG_INTERFACE_H
+
+#include <string>
+
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Rewrite/Core/Rewriter.h"
+
+class ClangInterface {
+public:
+	ClangInterface( std::string fileName );
+
+	void processAST( clang::ASTConsumer *astConsumer );
+	std::string getRewrittenCode();
+
+	clang::CompilerInstance &getCompilerInstance() {
+		return myCompilerInstance;
+	}
+	clang::Rewriter &getRewriter() {
+		return myRewriter;
+	}
+	clang::ASTContext &getASTContext() {
+		return myCompilerInstance.getASTContext();
+	}
+
+private:
+	clang::CompilerInstance myCompilerInstance;
+	clang::Rewriter myRewriter;
+	clang::TargetOptions TO;
+	clang::CompilerInvocation invocation;
+
+};
+
+#endif
